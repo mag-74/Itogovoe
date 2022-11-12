1d2c809594716492791a44747c8ffc8b8587e8ee
m_a_g_ <m_a_g_12378@mail.ru>
2022-11-12 22:36:21 +0500

    добавлено описание решения

--- Program.cs
+++ Program.cs
@@ -1,11 +1,10 @@
 ﻿// Текст задачи: Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше либо равна 3 символа.
 // Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.
 // При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
-
 Console.Clear();
-void ArrayIf(string[] array, string[] arraynew)
+void ArrayIf(string[] array, string[] arraynew) // Перещелкиваем весь массив, сравнивая каждое значение по его длине.
 {
-    int count = 0;
+    int count = 0; // "Count" - счетчик для второго массива.
     for (int i = 0; i < array.Length; i++)
     {
         if (array[i].Length <= 3)
@@ -15,8 +14,7 @@ void ArrayIf(string[] array, string[] arraynew)
         }
     }
 }
-
-void PrintArray(string[] array)
+void PrintArray(string[] array) // Распечатка массива в терминал
 {
     for (int i = 0; i < array.Length; i++)
     {
@@ -28,8 +26,8 @@ void PrintArray(string[] array)
 // "hello", "2", "world", ":-)" -> ["2", ":-)"]
 // "1234", "1567", "-2", "computer science" -> ["-2"]
 // "Russia", "Denmark", "Kazan" -> []
-string[] array = { "hello", "2", "world", ":-)" };
-string[] arraynew = new string[array.Length];
+string[] array = { "hello", "2", "world", ":-)" }; // Рисуем первый массив
+string[] arraynew = new string[array.Length]; // Создаем второй массив, контейнер после обработки первого
 Console.WriteLine("Первоначальный массив строк: ");
 PrintArray(array);
 ArrayIf(array, arraynew);
